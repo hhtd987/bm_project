53d5c005d44e54367866807069c7d0b4f3c6d13c
hhtd987 <hhtd.987@gmail.com>
2026-08-08 01:27:48 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
 print('website header')
-<<<<<<< HEAD
 print('picture update by hasan')
-=======
-print('picture changed by haneen')
->>>>>>> 5249b5a5a90369fa05401d5952f46508c5bf80ef
 print('add about section')
